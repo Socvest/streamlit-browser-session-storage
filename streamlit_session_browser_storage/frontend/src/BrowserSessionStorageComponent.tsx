b0ecd58d09fd18cc921c5ec7fb7cd73016e225a8
Socvest <82335390+Socvest@users.noreply.github.com>
2023-10-11 11:25:07 +0100

    Add files via upload

---
+++ streamlit_session_browser_storage/frontend/src/BrowserSessionStorageComponent.tsx
@@ -0,0 +1,85 @@
+import {
+  Streamlit,
+  StreamlitComponentBase,
+  withStreamlitConnection,
+  ComponentProps
+} from "streamlit-component-lib"
+import React, { useEffect, useMemo, useState } from "react"
+import useSessionStorageState from 'use-session-storage-state'
+
+const BrowserSessionStorageComponent:React.FC<ComponentProps> = (props:any) => {
+
+  const { args } = props 
+  const method:any = args["method"]
+  const itemKey:any = args["itemKey"]
+  const itemValue:any = args["itemValue"]
+  const sessionStrageOptions:any = args["sessionStrageOptions"] || { defaultValue: null }
+  const [storageItems, setStorageItems, {removeItem, isPersistent}] = useSessionStorageState<any>(itemKey, sessionStrageOptions)
+
+  const setItemF = (itemKey:any, itemValue:any) => {
+    let toSave = {item:itemKey, value:itemValue}
+    setStorageItems(toSave)
+  }  
+
+  const deleteAll = () => {
+    sessionStorage.clear();
+    return true
+  }
+
+  const getAll = () => {
+    const toSendToStreamlit = {...sessionStorage}
+    return toSendToStreamlit
+  }
+
+  const eraseItem = (itemKey:any) => {
+    sessionStorage.removeItem(itemKey)
+  }
+
+  useEffect(() => {
+    
+    switch(method){
+      case "getItem":
+
+        Streamlit.setComponentValue({"storage":storageItems, "dataPersist":isPersistent})
+        Streamlit.setComponentReady()
+       
+        break
+      case "setItem":
+
+        setItemF(itemKey, itemValue)
+        break
+      case "deleteItem":
+
+        removeItem()
+        break
+      case "eraseItem":
+        eraseItem(itemKey)
+        break
+      
+      case "deleteAll":
+        deleteAll()
+        break
+      
+      case "getAll":
+        const allStored = getAll()
+        Streamlit.setComponentValue(allStored)
+        Streamlit.setComponentReady()
+        break
+
+      default:
+        break
+    }
+
+
+  }, [method, setStorageItems, storageItems])
+  
+  
+  return ( 
+    <div style={{display:"none"}} >
+    </div>     
+  )
+}
+
+export default withStreamlitConnection(BrowserSessionStorageComponent)
+
+
---
+++ streamlit_session_browser_storage/frontend/src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import BrowserSessionStorageComponent from "./BrowserSessionStorageComponent"
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserSessionStorageComponent />
+  </React.StrictMode>,
+  document.getElementById("root")
+)
